c336991570472dca7a88efd073b566e5ac3c15c1
whuck <70232597+whuck@users.noreply.github.com>
2021-11-19 10:10:29 -0600

    done

---
+++ HuckFacts/HuckFacts/HuckFactData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HuckFacts
+{
+    public class HuckFactData
+    {
+        public HuckFactData()
+        {
+        }
+        
+        public static IEnumerable<HuckFactData> All { private set; get; }
+        public string TheFact { get; set; }
+        public string ShortFact { get; set; }
+        public string ImageFile { get; set; }
+        static HuckFactData()
+        {
+            List<String> imgs = new List<String>();
+            imgs.Add("one.png");
+            imgs.Add("two.png");
+            imgs.Add("three.png");
+            imgs.Add("four.png");
+            imgs.Add("five.png");
+            
+
+            List<HuckFactData> all = new List<HuckFactData>();
+            all.Add(new HuckFactData() { ImageFile = imgs[0], TheFact = "Hucks are of predominantly German Heritage.", ShortFact = "Huck Ancestry" });
+            all.Add(new HuckFactData() { ImageFile = imgs[1], TheFact = "Hucks are deceivingly average students, high aptitude & low attitude.", ShortFact = "Skool" });
+            all.Add(new HuckFactData() { ImageFile = imgs[2], TheFact = "Hucks love horseradish, mustard, and peppers.", ShortFact = "Spicey" });
+            all.Add(new HuckFactData() { ImageFile = imgs[3], TheFact = "A majority of Hucks live in the Midwestern United States", ShortFact = "Location" });
+            all.Add(new HuckFactData() { ImageFile = imgs[4], TheFact = "Hucks cannot live without a steady intake of caffeine.", ShortFact = "Coffee" });
+            All = all;
+
+            
+        }
+    }
+}
---
+++ HuckFacts/HuckFacts/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HuckFacts
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnSelection(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+            HuckFactData fact = (HuckFactData)e.SelectedItem;
+            DisplayAlert("The Fact", fact.TheFact, "Ok");
+        }
+    }
+}
